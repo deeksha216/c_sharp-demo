ec03ece1e1c86e3183eb488209bd68c16b13f64f
deeksha216 <deekshamoni216@gmail.com>
2025-09-19 10:42:57 +0530

    initial commit

---
+++ shapes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Shapes
+{
+    public virtual void Draw() => Console.WriteLine("dreaing a shape");
+    public virtual void erase() => Console.WriteLine("erasing the shape");
+}
+class Circle : Shapes
+{
+    public override void Draw() => Console.WriteLine("drawing the circle");
+    public override void erase() => Console.WriteLine("erasing the circle");
+}
+class Triangle : Shapes
+{
+    public override void Draw() => Console.WriteLine("drawing the triangle");
+    public override void erase() => Console.WriteLine("erasing the triangle"); 
+}
+class Square: Shapes
+{
+    public override void Draw() => Console.WriteLine("drawing the square");
+    public override void erase() => Console.WriteLine("erasing the square");
+
+}
+class Program
+{
+    public static void Main()
+    {
+        Shapes[] shapes = { new Circle(), new Triangle(), new Square() };
+        foreach (Shapes s in shapes)
+        {
+            s.Draw();
+            s.erase();
+        }
+    }
+}
---
+++ sumprog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Sum
+{
+    static void Main()
+    {
+        int n, sum = 0;
+        Console.WriteLine("Enter a positive integer: ");
+        n = Convert.ToInt32(Console.ReadLine());
+         while(n>0)
+        {
+            sum = sum + (n % 10);
+            n = n / 10;
+
+        }
+         Console.WriteLine("sum of digits is: " + sum);
+    }
+}
