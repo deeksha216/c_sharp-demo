8fe395d9260eab321eee9bc5b28558841e71906b
deeksha216 <deekshamoni216@gmail.com>
2025-09-19 10:32:29 +0530

    Initial Commit

---
+++ interfacecamera/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+    interface Camera
+    {
+        void capturePhoto();
+    }
+    interface Printer
+    {
+        void printDocument();
+    } 
+class Myclass: Camera ,Printer
+{
+    public void capturePhoto()
+    {
+        Console.WriteLine("Photo captured.");
+    }
+    public void printDocument()
+    {
+        Console.WriteLine("Document printed.");
+    }
+}
+class Program
+{ 
+    static void Main(string[] args)
+{
+    Myclass d = new Myclass();
+    d.capturePhoto();
+    d.printDocument();
+}
+}
+
+
+  
+
+   
