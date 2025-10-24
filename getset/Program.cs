ecc242fa39bd6b173025a59eba009484aa40c32a
deeksha216 <deekshamoni216@gmail.com>
2025-10-24 09:34:51 +0530

    Initial commit

---
+++ getset/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Student
+{
+    private string name;
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+}
+class program
+{
+    static void Main()
+    {
+        Student s = new Student();
+        s.Name = "dia";
+        Console.WriteLine("student name is :"+s.Name);
+    }
+
+}
